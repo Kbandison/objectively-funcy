dc1cb9df7df82d3e0a9c3cfb8575e159b0afd5f5
Kevin Bandison <kbandison@gmail.com>
2022-10-12 15:55:56 -0400

    completed assignment

--- main.js
+++ main.js
@@ -1,8 +1,41 @@
 // Your code here.
 
+function getFirstName(person) {
+  return person.firstName;
+}
 
+function getLastName(person) {
+  return person.lastName;
+}
 
+function getFullName(person) {
+  return `${person.firstName} ${person.lastName}`;
+}
 
+function setFirstName(person, newName) {
+  person.firstName = newName;
+}
+
+function setAge(person, newAge) {
+  person.age = newAge;
+}
+
+function giveBirthday(person) {
+  if(person.age === undefined){
+    person.age = 1;
+  } else{
+  person.age += 1;
+  }
+}
+
+function marry(person1, person2){
+    if (person1.married === false || person2.married === false){
+      person1.married = true;
+      person2.married = true;
+      person1.spouseName = `${person2.firstName} ${person2.lastName}`;
+      person2.spouseName = `${person1.firstName} ${person1.lastName}`;
+  }
+}
 
 // Our code here. Don't touch!
 if (typeof getFirstName === 'undefined') {
